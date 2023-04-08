50878772e656ba7a41479171b5225ec420547313
Admin <Admin@DESKTOP-H5L639L>
2023-04-08 21:03:20 +0530

    newcommit

---
+++ src/numberSystem/decimaltoHexadecimal.java
@@ -0,0 +1,15 @@
+package numberSystem;
+
+public class decimaltoHexadecimal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(Integer.toHexString(245));  
+		System.out.println(Integer.toHexString(45));  
+		System.out.println(Integer.toHexString(67));  
+		System.out.println(Integer.toHexString(98));  
+		System.out.println(Integer.toHexString(712));   
+
+	}
+
+}
---
+++ src/numberSystem/decimaltooctal.java
@@ -0,0 +1,19 @@
+package numberSystem;
+
+public class decimaltooctal {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		
+		System.out.println(Integer.toOctalString(20));  
+		System.out.println(Integer.toOctalString(100));  
+		System.out.println(Integer.toOctalString(56));  
+		System.out.println(Integer.toOctalString(78));  
+		System.out.println(Integer.toOctalString(1453));  
+		System.out.println(Integer.toOctalString(544));  
+
+
+	}
+
+}
