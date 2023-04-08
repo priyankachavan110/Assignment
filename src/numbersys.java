5914f345f76c92651d0e67e2705824405e5a2ba8
Admin <Admin@DESKTOP-H5L639L>
2023-04-08 20:57:26 +0530

    Revert "new"
    This reverts commit 8862977944d2b3445ff0951b8a333d6ad6f09440.

--- src/numbersys.java
+++ src/numbersys.java
@@ -1,65 +1,8 @@
 
 public class numbersys {
 
-	public static void main(String[] args) 
-	{
+	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-		/*Covert below Decimal to Octal
-		 * 20
-		 * 100
-		 * 56
-		 * 78
-		 * 1453
-		 * 544
-		 */
-        System.out.println("Decimal to Octal");
-
-		int k = 020; //  octal 
-		System.out.println(k);
-
-		k= 0100;
-		System.out.println(k);
-		
-		k= 056;
-		System.out.println(k);
-		
-		k=78;
-		System.out.println(k);
-		
-		k= 01453;
-		System.out.println(k);
-		
-		k= 0544;
-		System.out.println(k);
-		
-		/*Covert below Decimal to Hexadecimal
-		 * 245
-		 * 45
-		 * 67
-		 * 98
-		 * 712
-*/
-        System.out.println("Decimal to Hexadecimal");
-
-
-		int m = 0X245; // Hexadecimal
-		System.out.println(m);
-		
-		m = 0X245; // Hexadecimal
-		System.out.println(m);
-		
-		m = 0X45; // Hexadecimal
-		System.out.println(m);
-		
-		m = 0X67; // Hexadecimal
-		System.out.println(m);
-		
-		m = 0X98; // Hexadecimal
-		System.out.println(m);
-
-		m = 0X712; // Hexadecimal
-		System.out.println(m);
 
 	}
 
