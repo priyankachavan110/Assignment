54362b6cc1e1a2aa81d95f65c4eb7bb04bb3ba09
Admin <Admin@DESKTOP-H5L639L>
2023-04-08 22:51:44 +0530

    decimal to binary

--- src/numberSystem/decimaltoBinary.java
+++ src/numberSystem/decimaltoBinary.java
@@ -4,7 +4,12 @@ public class decimaltoBinary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(Integer.toBinaryString(20));
+		System.out.println(Integer.toBinaryString(39));
+		System.out.println(Integer.toBinaryString(49));
+		System.out.println(Integer.toBinaryString(28));
+		System.out.println(Integer.toBinaryString(126));
+		System.out.println(Integer.toBinaryString(156));
 	}
 
 }
